9f70d438394ea0abdd734fa7fc60d7b72d58b534
alieuidsh <127293456+alieuidsh@users.noreply.github.com>
2023-03-10 15:34:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message_text = " (我是AI胖胖)"
+    message = TextSendMessage(text=message_text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
