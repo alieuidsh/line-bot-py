4c47fcae805e9e5005b81dfa0ac53edbb1377d93
alieuidsh <127293456+alieuidsh@users.noreply.github.com>
2023-03-10 15:57:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def callback():
 def handle_message(event):
     #message_text = AliGPT.CH_GPT(event.message.text)
     #message_text = " (我是AI胖胖)"
-    message_text = " (我是AI胖胖)"
+    message_text = " 我是AI胖胖"
     message = TextSendMessage(text=message_text)
     line_bot_api.reply_message(event.reply_token, message)
 
