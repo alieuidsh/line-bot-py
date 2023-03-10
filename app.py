a209365f2d8a21458ee85c6b519370b8a9ed620e
alieuidsh <a0908427099@gmail.com>
2023-03-10 14:06:34 +0800

    init

--- app.py
+++ app.py
@@ -25,28 +25,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    user_input = event.message.text
-    
-    # set up the OpenAI prompt with user input
-    prompt = f"User: {user_input}\nAI:"
-    
-    # generate AI response using OpenAI's GPT model
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    
-    # get the AI response text from the OpenAI response
-    ai_response = response.choices[0].text.strip()
-    
-    # send the AI response back to the user
-    message = TextSendMessage(text=ai_response)
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-    
 
 import os
 if __name__ == "__main__":
