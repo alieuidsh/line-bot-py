27d7fdfd6b16fdfce46307e06e93e7c11c57ccd9
alieuidsh <127293456+alieuidsh@users.noreply.github.com>
2023-03-10 15:26:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-#import openai
 import os
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-#openai.api_key = os.environ['openai.api_key']
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -23,10 +23,11 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-    message = str(TextSendMessage(text=event.message.text))
-    message = '我是AI胖胖' + message
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
-    
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
