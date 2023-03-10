1424178d1f48ac815665c23be681d5ca6679feca
alieuidsh <127293456+alieuidsh@users.noreply.github.com>
2023-03-10 14:15:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-#import openai
+import openai
 import os
 
 app = Flask(__name__)
